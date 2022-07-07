7e488300cf92764fcdc5401666b674189b40f005
Tevin026 <106956161+Tevin026@users.noreply.github.com>
2022-07-07 18:58:31 +0200

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,16 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * mul - Multiply to integers
+ *
+ * @a: interger one
+ * @b: interger two
+ *
+ * Return: Proudct of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
